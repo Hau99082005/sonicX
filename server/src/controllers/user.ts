52d41384f90db3e572c36acb82f4d7fd8d7de18c
Hau99082005 <hau99082005@gmail.com>
2026-05-01 22:12:11 +0700

    main

---
+++ server/src/controllers/user.ts
@@ -0,0 +1,33 @@
+import { CreateUser } from "#/@types/user";
+import User from "#/models/User";
+import { CreateUserSchema } from "#/utils/validationSchema";
+import bcrypt from "bcryptjs";
+import { RequestHandler } from "express";
+
+export const create: RequestHandler = async (req: CreateUser, res) => {
+    try {
+        const { name, email, password } = req.body;
+        CreateUserSchema.validate({ email, name, password })
+            .catch(error => {
+
+            });
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        return res.status(201).json({
+            message: "Tạo user thành công",
+            user: newUser
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal Server Error",
+            error
+        });
+    }
+}
--- server/src/router/auth.ts
+++ server/src/router/auth.ts
@@ -1,40 +1,10 @@
-import { CreateUser } from "#/@types/user";
-import User from "#/models/User";
 import { Router } from "express";
-import bcrypt from "bcryptjs";
 import { CreateUserSchema } from "#/utils/validationSchema";
 import { validate } from "#/middleware/validator";
+import { create } from "#/controllers/user";
 
 const router = Router();
 
-router.post('/create',
-    validate(CreateUserSchema),
-    async (req: CreateUser, res) => {
-        try {
-            const { name, email, password } = req.body;
-            CreateUserSchema.validate({ email, name, password })
-                .catch(error => {
-
-                });
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash(password, salt);
-
-            const newUser = await User.create({
-                name,
-                email,
-                password: hashedPassword
-            });
-
-            return res.status(201).json({
-                message: "Tạo user thành công",
-                user: newUser
-            });
-        } catch (error) {
-            return res.status(500).json({
-                message: "Internal Server Error",
-                error
-            });
-        }
-    });
+router.post('/create', validate(CreateUserSchema), create);
 
 export default router;
