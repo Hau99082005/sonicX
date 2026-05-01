c3d40161744ac9ee341e37266ec0a60ebd0d19d8
Hau99082005 <hau99082005@gmail.com>
2026-05-01 21:32:53 +0700

    main

--- server/src/router/auth.ts
+++ server/src/router/auth.ts
@@ -1,37 +1,40 @@
+import { CreateUser } from "#/@types/user";
 import User from "#/models/User";
 import { Router } from "express";
 import bcrypt from "bcryptjs";
+import { CreateUserSchema } from "#/utils/validationSchema";
+import { validate } from "#/middleware/validator";
 
 const router = Router();
 
-router.post('/create', async (req, res) => {
-    try {
-        const { name, email, password } = req.body; 
-        if(!name || !email || !password) {
-            return res.status(400).json({
-                message: "Invalid required!"
+router.post('/create',
+    validate(CreateUserSchema),
+    async (req: CreateUser, res) => {
+        try {
+            const { name, email, password } = req.body;
+            CreateUserSchema.validate({ email, name, password })
+                .catch(error => {
+
+                });
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            const newUser = await User.create({
+                name,
+                email,
+                password: hashedPassword
+            });
+
+            return res.status(201).json({
+                message: "Tạo user thành công",
+                user: newUser
+            });
+        } catch (error) {
+            return res.status(500).json({
+                message: "Internal Server Error",
+                error
             });
         }
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        const newUser = await User.create({
-            name,
-            email,
-            password: hashedPassword
-        });
-
-        return res.status(201).json({
-            message: "Tạo user thành công",
-            user: newUser
-        });
-    } catch (error) {
-        return res.status(500).json({
-            message: "Internal Server Error",
-            error
-        });
-    }
-});
+    });
 
 export default router;
