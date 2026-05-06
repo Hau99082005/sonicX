3d0bab0e66f41d1c01c8add4b9afbb17a0394932
Hau99082005 <hau99082005@gmail.com>
2026-05-06 23:21:08 +0700

    main

--- server/src/controllers/playlist.ts
+++ server/src/controllers/playlist.ts
@@ -1,4 +1,4 @@
-import { CreatePlaylist, UpdatePlaylist } from "#/@types/audio";
+import { CreatePlaylist, PopulateFavList, UpdatePlaylist } from "#/@types/audio";
 import Audio from "#/models/audio";
 import Playlist from "#/models/playlist";
 import { RequestHandler } from "express";
@@ -99,3 +99,40 @@ export const getPlaylistByIdProfile: RequestHandler = async (req, res) => {
 
     res.json({ playlist });
 };
+
+export const getAudios: RequestHandler = async (req, res) => {
+    const { playlistId } = req.params;
+    if (!isValidObjectId(playlistId)) return res.status(422).json({ error: "Invalid playlistId!" });
+
+    const playlist = await Playlist.findOne({
+        owner: req.user.id,
+        _id: playlistId
+    }).populate<{ items: PopulateFavList[] }>({
+        path: "items", populate: {
+            path: "owner",
+            select: "name",
+        }
+    });
+    if (!playlist) return res.json({ list: [] })
+    const audios = playlist?.items.map((item) => {
+        return {
+            id: item._id,
+            title: item.title,
+            category: item.category,
+            file: item.file.url,
+            poster: item.poster?.url,
+            owner: {
+                name: item.owner.name,
+                id: item.owner._id
+            }
+        }
+    })
+    res.json({
+        list: {
+            id: playlist._id,
+            title: playlist.title,
+            audios,
+        }
+    })
+
+}
