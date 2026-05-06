cb8e6e0915d3f92b423e6fe2108762a10d31e4f3
Hau99082005 <hau99082005@gmail.com>
2026-05-07 00:11:05 +0700

    main

--- server/src/controllers/follower.ts
+++ server/src/controllers/follower.ts
@@ -74,3 +74,19 @@ export const getPublicUploads: RequestHandler = async (req, res) => {
     })
     res.json({ audios });
 }
+
+export const getPublicProfile: RequestHandler = async (req, res) => {
+    const { profileId } = req.params;
+    if (!isValidObjectId(profileId)) return res.status(422).json({ error: "Invalid profile Id!" });
+    const user = await User.findById(profileId);
+    if (!user) return res.status(404).json({ error: "User not found!" });
+
+    res.json({
+        profile: {
+            id: user._id,
+            name: user.name,
+            followers: user.followers.length,
+            avatar: user.avatar?.url
+        }
+    })
+}
--- server/src/router/profile.ts
+++ server/src/router/profile.ts
@@ -1,4 +1,4 @@
-import { getPublicUploads, getUploads, updatedFollower } from "#/controllers/follower";
+import { getPublicProfile, getPublicUploads, getUploads, updatedFollower } from "#/controllers/follower";
 import { mustAuth } from "#/middleware/auth";
 import { Router } from "express";
 
@@ -7,5 +7,6 @@ const router = Router();
 router.post("/update-follower/:profileId", mustAuth, updatedFollower);
 router.get('/uploads', mustAuth, getUploads);
 router.get("/uploads/:profileId", getPublicUploads);
+router.get("/info/:profileId", getPublicProfile);
 
 export default router;
