2f99d8ef6851ec6fa8862e2639c4b6a9eb5b0eec
Hau99082005 <hau99082005@gmail.com>
2026-05-07 19:39:14 +0700

    main

--- server/src/router/history.ts
+++ server/src/router/history.ts
@@ -1,4 +1,4 @@
-import { removeHistory, updateHistory } from "#/controllers/history";
+import { getHistories, removeHistory, updateHistory } from "#/controllers/history";
 import { mustAuth } from "#/middleware/auth";
 import { validate } from "#/middleware/validator";
 import { updatedHistorySchema } from "#/utils/validationSchema";
@@ -8,5 +8,6 @@ import { Router } from "express";
 const router = Router();
 router.post("/", mustAuth,validate(updatedHistorySchema) ,updateHistory);
 router.delete("/",mustAuth, removeHistory);
+router.get("/", mustAuth, getHistories);
 
 export default router;
