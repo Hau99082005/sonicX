0bb9bc82e1baad1b733b43a7799463d041213e58
Hau99082005 <hau99082005@gmail.com>
2026-05-09 09:29:31 +0700

    main

--- server/src/utils/schedule.ts
+++ server/src/utils/schedule.ts
@@ -6,15 +6,15 @@ const generatedPlaylist = async () => {
     const result = await Audio.aggregate([
         { $sort: { likes: -1 } },
         {
+            $sample: { size: 20 },
+        },
+        {
             $group: {
                 _id: "$category",
                 audios: {
                     $push: "$$ROOT._id"
                 },
             },
-        },
-        {
-            $limit: 20,
         },
     ]);
     result.map(async (item) => {
