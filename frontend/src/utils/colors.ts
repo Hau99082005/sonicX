d49c88dff82b2292629e5671b27505bfbeebf780
Hau99082005 <hau99082005@gmail.com>
2026-05-10 16:50:25 +0700

    main

---
+++ frontend/src/utils/colors.ts
@@ -0,0 +1,21 @@
+const PRIMARY = '#0A0E21';
+const SECONDARY = '#6C63FF';
+const BLUE = '#449df6';
+const CONTRAST = '#FFFFFF';
+const INACTIVE_CONTRAST = 'rgba(255, 255, 255, 0.4)';
+const ERROR = '#FF5B5B';
+const SUCCESS = '#3BEF1E';
+const OVERLAY = 'rgba(10, 14, 33, 0.7)';
+
+const colors = {
+  PRIMARY,
+  SECONDARY,
+  BLUE,
+  CONTRAST,
+  INACTIVE_CONTRAST,
+  ERROR,
+  SUCCESS,
+  OVERLAY,
+};
+
+export default colors;
